1e91e3e17c2a238c8a410a43ce3f5313e443f42d
GrzegorzStacel <stacelgrzegorz@gmail.com>
2024-09-14 07:00:48 +0200

    Refactor filtering functions and improve output display

--- Arrays/evenNumber.ts
+++ Arrays/evenNumber.ts
@@ -1,8 +1,6 @@
-export function evenNumber(): void {
-  // Napisz funkcję, która przyjmuje tablicę liczb i zwraca nową tablicę zawierającą tylko liczby parzyste.
-  const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  console.log(filterEvenNumbers(nums)); // [2, 4, 6, 8, 10]
-}
+import chalk from "chalk";
+
+// Napisz funkcję, która przyjmuje tablicę liczb i zwraca nową tablicę zawierającą tylko liczby parzyste.
 
 function filterEvenNumbers(numbers: number[]): number[] {
   let tmpArray: number[] = [];
@@ -12,4 +10,39 @@ function filterEvenNumbers(numbers: number[]): number[] {
   });
 
   return tmpArray;
+}
+
+function filterEvenNumbersByFilter(numbers: number[]): number[] {
+  return numbers.filter((number) => number % 2 === 0);
+}
+
+function displayResult(result: number[]): void {
+  console.dir(result, { maxArrayLength: 10 });
+  console.log(`Liczba parzystych elementów: ${result.length}\n`);
+}
+
+function measureFilteringPerformance(array: number[], filterType: "forEach" | "filter"): void {
+  console.log(chalk.green(`Tablica (${array.length}) elementów, używa metody: ${filterType}`));
+
+  const filterFunction = filterType === "forEach" ? filterEvenNumbers : filterEvenNumbersByFilter;
+
+  console.time(chalk.yellow(`Czas dla metody ${filterType}`));
+  const result = filterFunction(array);
+  console.timeEnd(chalk.yellow(`Czas dla metody ${filterType}`));
+
+  displayResult(result);
+}
+
+export function evenNumber(smallArray: number[], veryLongArray: number[]): void {
+  console.log(chalk.blue('\n\nRozpoczynam zadanie "evenNumber"\n'));
+
+  // Wywołanie dla małej tablicy z różnymi metodami filtrowania
+  measureFilteringPerformance(smallArray, "forEach");
+  measureFilteringPerformance(smallArray, "filter");
+
+  // Wywołanie dla dużej tablicy z różnymi metodami filtrowania
+  measureFilteringPerformance(veryLongArray, "forEach");
+  measureFilteringPerformance(veryLongArray, "filter");
+
+  console.log(chalk.blue("Koniec zadania 'evenNumber'\n\n\n"));
 }
--- index.ts
+++ index.ts
@@ -3,7 +3,10 @@ import { findMax } from "./Arrays/findMax.js";
 import { sum } from "./Arrays/sum.js";
 import { sortByName } from "./Arrays/sortByName.js";
 
-// evenNumber();
+const smallArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const veryLongArray = Array.from({ length: 1000000 }, (_, value) => value + 1);
+
+evenNumber(smallArray, veryLongArray);
 // findMax();
 // sum();
 sortByName();
