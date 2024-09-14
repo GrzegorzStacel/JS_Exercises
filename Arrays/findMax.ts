1fa161ce008b2158b2bd7483fec9b30e29ac68c0
GrzegorzStacel <stacelgrzegorz@gmail.com>
2024-09-14 13:40:41 +0200

    Refactoring. Implement helpers functions & improve output styles to better readable.

--- Arrays/findMax.ts
+++ Arrays/findMax.ts
@@ -1,5 +1,17 @@
-function findMaxLoop(array: number[]): number {
-  console.time("czas - findMaxLoop");
+//
+//
+// Napisz funkcję, która znajdzie największą liczbę w podanej tablicy.
+//
+//
+
+import chalk from "chalk";
+import { measureFunctionTime } from "../helpers/measureFunctionTime.js";
+import { displayResult } from "../helpers/displayResult.js";
+import { validateArray } from "../helpers/validateArray.js";
+
+function findMaxForLoop(array: number[]): number {
+  validateArray(array);
+
   let max = array[0];
 
   for (let i = 1; i < array.length; i++) {
@@ -8,26 +20,41 @@ function findMaxLoop(array: number[]): number {
     }
   }
 
-  console.timeEnd("czas - findMaxLoop");
   return max;
 }
 
 function findMaxMath(array: number[]): number {
-  console.time("czas - findMaxMath");
+  validateArray(array);
+
   const number = Math.max(...array);
-  console.timeEnd("czas - findMaxMath");
   return number;
 }
 
-export function findMax(): void {
-  // Napisz funkcję, która znajdzie największą liczbę w podanej tablicy.
-  const numbers = [10, 25, 3, 99, 47, 12];
+function measureFindMaxPerfomance(array: number[], findType: "ForLoop" | "Math"): void {
+  console.log(chalk.blue(`\n\nTablica (${array.length}) elementów, używając metody: ${findType}`));
 
-  console.log("findMaxLoop: ", findMaxLoop(numbers)); // 99 (0.162ms)
-  console.log("findMaxMath: ", findMaxMath(numbers)); // 99 (0.137ms)
+  const findTypeFunction = findType === "ForLoop" ? findMaxForLoop : findMaxMath;
+
+  const result = measureFunctionTime(findTypeFunction, array, `Czas dla metody ${findType}`);
+
+  displayResult(`Największa liczba: ${result}`);
+}
+
+export function findMax(): void {
+  const numbers = [10, 25, 3, 99, 47, 12];
+  const mockArray: number[] = Array.from({ length: 115000 }, (_, i) => i + 1);
+
+  console.log(chalk.greenBright.bold("\n\nNapisz funkcję, która znajdzie największą liczbę w podanej tablicy."));
+  console.log(chalk.underline.greenBright("Rozpoczynam zadanie 'findMax:'"));
+
+  measureFindMaxPerfomance(numbers, "ForLoop"); // 99 (0.162ms)
+  measureFindMaxPerfomance(numbers, "Math"); // 99 (0.137ms)
 
   // Wieksze liczby wywołują błąd z Math.max() z powodu przekroczenia maksymalnego rozmiaru (przynajmniej na mojej maszynie)
-  const mockArray: number[] = Array.from({ length: 115000 }, (_, i) => i + 1);
-  console.log("findMaxLoop: ", findMaxLoop(mockArray)); // 115000 (2.083ms)
-  console.log("findMaxMath: ", findMaxMath(mockArray)); // 115000 (1.399ms)
+  measureFindMaxPerfomance(mockArray, "ForLoop"); // 115000 (2.083ms)
+  measureFindMaxPerfomance(mockArray, "Math"); // 115000 (1.399ms)
+
+  // measureFindMaxPerfomance([], "ForLoop"); // Test dla validateArray()
+
+  console.log(chalk.red.underline("\nKoniec zadania 'findMax'\n"));
 }
